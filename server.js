9eccd05896bb2762aceeab3298a6bc57524fcdba
giordifungula <jodi.fungula@gmail.com>
2019-06-08 21:09:44 +0200

    added the views and server file

---
+++ server.js
@@ -0,0 +1,36 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").load();
+  // require the env
+}
+let express = require("express");
+
+let app = express();
+
+let expressLayouts = require("express-ejs-layouts");
+
+let indexRouter = require("./routes/index");
+// index will need to be imported to the application
+
+// Configure the application
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+// connect to database
+let mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true
+  //   Mongoose uses a older way that is deprecated so you need this
+});
+// Check if connected
+let db = mongoose.connection();
+db.on("error", error => console.log(error));
+db.once("open", () => console.log("Connected to Mongoose"));
+
+// index of application
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 3000);
