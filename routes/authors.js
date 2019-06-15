504ce029294a5f4857964341f31f60c6a610b71d
giordifungula <jodi.fungula@gmail.com>
2019-06-15 22:01:17 +0200

    new route and form included working well

--- routes/authors.js
+++ routes/authors.js
@@ -5,18 +5,16 @@ let router = express.Router();
 let Author = require("../models/author");
 
 // ALl Authors Route
-
 router.get("/", (req, res) => {
   res.render("authors/index");
 });
 
 // New Author route
 router.get("/new", (req, res) => {
-  res.render("authors/new"),
-    {
-      author: new Author()
-      // creates a new author from database
-    };
+  res.render("authors/new", {
+    author: new Author()
+    // creates a new author from database
+  });
 });
 
 // Creating th author route
